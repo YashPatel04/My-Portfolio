daa3c7faa3522c942fa54d6ff2cefae0fad3d9e7
Yash Patel <yashbpatel2004@gmail.com>
2024-09-27 11:09:43 -0400

    minor changes

--- src/components/content/Project.js
+++ src/components/content/Project.js
@@ -9,11 +9,13 @@ function Project() {
         <ul id='pi-1' className='project-item'>
             <Projectcard
               className="Ind-card"
+              link='https://github.com/YashPatel04/Library-Management-System'
               title="Library Management System"
               text="Designed and implemented a comprehensive Library Management System using Python, Tkinter, and MySQL, enabling  management of library resources and user transactions."
             />
             <Projectcard
               className="Ind-card"
+              link='https://github.com/YashPatel04/Face-Detection-System'
               title="Face Detection System"
               text="Developed a robust face detection system in Java using the OpenCV library, implementing advanced image processing algorithms to accurately identify and track faces in real-time, ensuring efficient and reliable recognition under various conditions."
             />
@@ -22,16 +24,19 @@ function Project() {
             <Projectcard
               className="Ind-card"
               title="Business Portfolio Website"
+              link='https://github.com/YashPatel04/business-portfolio-website'
               text="Created a sample business portfolio website using modern web technologies to showcase business services, projects, and contact information in a professional and visually appealing manner."
             />
             <Projectcard
               className="Ind-card"
               title="Personal Portfolio Website"
+              link='https://github.com/YashPatel04/My-Portfolio'
               text="Developed an interactive portfolio website using React and GSAP, featuring smooth animations and dynamic content. Added email functionality with the Gmail API and ensured a responsive design for all devices."
             />
             <Projectcard
               className="Ind-card"
               title="Java Game Development (Team Project)"
+              link='https://github.com/CS-142-Spring/team-cyber-ware'
               text="Collaborated on the development of “J.T.S.M” focusing on core game engine, user interface, gameplay mechanics, mission planning, and integration with external APIs like DallE for image generation."
             />
           </ul>
